60100d964a8e4e1e1c0e24326c07d236a0c9a862
Philippe Bogaerts <xxradar@Philippes-MacBook-Pro-2.local>
2020-05-06 19:49:28 +0200

    Update folder naming

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 
 git clone https://github.com/xxradar/app_routable_demo.git
 cd ./app_routable_demo
-kubectl create ns routable-app-demo
-kubectl apply -n routable-app-demo -f ./nginx-conf-map.yaml
-kubectl apply -n routable-app-demo -f ./zone_svc.yaml
-kubectl apply -n routable-app-demo -f ./echoserver1.yaml
-kubectl apply -n routable-app-demo -f ./echoserver2.yaml
-kubectl apply -n routable-app-demo -f ./nginx-zone.yaml
-kubectl get po -n routable-app-demo
+kubectl create ns app_routable_demo
+kubectl apply -n app_routable_demo -f ./nginx-conf-map.yaml
+kubectl apply -n app_routable_demo -f ./zone_svc.yaml
+kubectl apply -n app_routable_demo -f ./echoserver1.yaml
+kubectl apply -n app_routable_demo -f ./echoserver2.yaml
+kubectl apply -n app_routable_demo -f ./nginx-zone.yaml
+kubectl get po -n app_routable_demo
 
-echo 'kubectl run -it -n routable-app-demo --rm --image xxradar/hackon mycurler -- bash'
+echo 'kubectl run -it -n app_routable_demo --rm --image xxradar/hackon mycurler -- bash'
