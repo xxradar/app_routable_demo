45b244cc6c7f0eec393fa3c631c926e2d9b7fee8
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2022-02-02 20:51:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 kubectl create ns app-routable-demo
-kubectl apply -n app-routable-demo -f ./nginx-conf-map.yaml
-kubectl apply -n app-routable-demo -f ./zone_svc.yaml
-kubectl apply -n app-routable-demo -f ./echoserver1.yaml
-kubectl apply -n app-routable-demo -f ./echoserver2.yaml
-kubectl apply -n app-routable-demo -f ./nginx-zone.yaml
-kubectl apply -n app-routable-demo -f ./siege.yaml
+kubectl apply -n app-routable-demo -f ./app/nginx-conf-map.yaml
+kubectl apply -n app-routable-demo -f ./app/zone_svc.yaml
+kubectl apply -n app-routable-demo -f ./app/echoserver1.yaml
+kubectl apply -n app-routable-demo -f ./app/echoserver2.yaml
+kubectl apply -n app-routable-demo -f ./app/nginx-zone.yaml
+kubectl apply -n app-routable-demo -f ./app/siege.yaml
 kubectl get po -n app-routable-demo
 
 echo 'Run for example: kubectl run -it -n app-routable-demo --rm --image xxradar/hackon mycurler -- bash'
